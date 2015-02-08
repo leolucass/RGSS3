bb389b25ba635b9f8f6b65d9fd7ab833a3881649
Masked1121232 <masked.rm@gmail.com>
2015-02-08 12:48:52 -0200

    Update [System]Survivor.rb

--- [System]Survivor.rb
+++ [System]Survivor.rb
@@ -16,18 +16,19 @@
 #--------------- #
 module EnterConfigHUD
 
-    Fome  = 199 #, Here are defined the values to Fome/Hunger.
+  Fome  = 199 #, Here are defined the values to Fome/Hunger.
 
-    Mfome = 200 # Here are defined the values to Max Fome/Max Hunger.
+  Mfome = 200 # Here are defined the values to Max Fome/Max Hunger.
     
-    Stamina = 199
+  Stamina = 199
     
-    Mstamina = 200
-  end
+  Mstamina = 200
+end
 
 #################################################
 class Game_BattlerBase
-  attr_accessor :fome, :mfome, :stamina, :mstamina
+  attr_reader :fome, :stamina
+  attr_accessor :mfome, :mstamina
 
   alias alsintlzgmactr initialize
 
@@ -38,171 +39,164 @@ class Game_BattlerBase
     @stamina = EnterConfigHUD::Stamina
     @mstamina = EnterConfigHUD::Mstamina
   end
+
+  def fome=(x)
+    @fome = x
+    @fome = @mfome if @fome > @mfome
+    @fome = 0 if @fome < 0
+  end
+
+  def stamina=(x)
+    @stamina = x
+    @stamina = @mstamina if @stamina > @mstamina
+    @stamina = 0 if @stamina < 0
+  end
 ##################################################################
 end
-class Scene_Map < Scene_Base
-  
-	alias enter_hud_start start
-	alias enter_hud_update update
-	alias enter_hud_terminate terminate
 
-	def start
-		enter_hud_start #alias
-		@actor = $game_party.members[0]
-		create_hud
-		update_variables
-	end
-	
-	def create_hud
-		create_sprites
-		create_bitmaps
-		get_bitmap_size
-		draw_hp
-		draw_mp
-		draw_fome
-    draw_stamina
-	end
-	
-	def update_variables
-		@actor_hp = @actor.hp
-		@actor_mhp = @actor.mhp
-		@actor_mp = @actor.mp
-		@actor_mmp = @actor.mmp
+class HUD
+
+  attr_accessor :actor
+
+  def initialize(actor)
+    @actor = actor
+    @bg = Sprite.new
+    @bg.bitmap = Bitmap.new(Graphics.width, Graphics.height)
+    @fg = Sprite.new
+    @fg.bitmap = Bitmap.new(Graphics.width, Graphics.height)
+    update_variables
+    draw_background
+    draw_foreground
+  end
+
+  def update
+    refresh if need_update?
+  end
+
+  def refresh
+    update_variables
+    draw_foreground
+  end
+
+  def draw_background
+    @bg.bitmap.clear
+    create_background_bitmaps
+    create_background
+    @bg.bitmap.blt(190, 350, @background, @background.rect)
+  end
+  
+  def draw_foreground
+    @fg.bitmap.clear
+    create_foreground_bitmaps
+    create_foreground
+    @fg.bitmap.blt(190, 350
+  end
+  
+  def create_background_bitmaps
+    @bhp = Cache.picture('barraenergiaCompleta')
+    @bmp = Cache.picture('barraenergiaCompleta')
+    @bfome = Cache.picture('barraenergiaCompleta')
+    @bstamina = Cache.picture('barraenergiaCompleta')
+    @background = Cache.picture('backgroundHUD')
+  end
+
+  def create_background
+    @background.blt(16, 12, @bstamina, @bstamina.rect)
+    @background.blt(16, 28, @bfome, @bfome.rect)
+    @background.blt(16, 44, @bhp, @bhp.rect)
+    @background.blt(16, 60, @bmp, @bmp.rect)
+  end
+
+  def create_foreground_bitmaps
+    @fhp = Cache.picture "hp_bar"
+    @fmp = Cache.picture "mp_bar"
+    @ffome = Cache.picture "fome_bar"
+    @fstamina = Cache.picture "stamina_bar"
+    @foreground = Bitmap.new(@background.width, @background.height)
+  end
+
+  def create_foreground
+    w = @fstamina.width * @actor.stamina/@actor.mstamina.to_f #Define the width of bar
+    h = @fstamina.height
+    rect = Rect.new(0,0,w.to_i,h)
+    @foreground.blt(16, 12, @fstamina, rect)
+    
+    w = @ffome.width * @actor.fome/@actor.mfome.to_f #Define the width of bar
+    h = @ffome.height
+    rect = Rect.new(0,0,w.to_i,h)
+    @foreground.blt(16, 28, @ffome, rect)
+
+    w = @fhp.width * @actor.hp/@actor.mhp.to_f #Define the width of bar
+    h = @fhp.height
+    rect = Rect.new(0,0,w.to_i,h)
+    @foreground.blt(16, 44, @fhp, rect)
+
+    w = @fmp.width * @actor.mp/@actor.mmp.to_f #Define the width of bar
+    h = @fmp.height
+    rect = Rect.new(0,0,w.to_i,h)
+    @foreground.blt(16, 60, @fmp, rect)
+  end
+
+  def update_variables
+    @actor_hp = @actor.hp
+    @actor_mhp = @actor.mhp
+    @actor_mp = @actor.mp
+    @actor_mmp = @actor.mmp
     @actor_fome = @actor.fome
     @actor_mfome = @actor.mfome
     @actor_stamina = @actor.stamina
     @actor_mstamina = @actor.mstamina
+  end
 
-		
-	end
-	
-	def create_sprites
-		@hp = Sprite.new
-		@mp = Sprite.new
-		@fome_bar = Sprite.new
-    @stamina_bar = Sprite.new
-    @backgroundHUD = Sprite.new
-	end
-	def create_bitmaps
-		@hp.bitmap = bitmap = Cache.picture('barraenergiaCompleta')#Verificar isso, quando apaga ela e coloca outra, as barras não aparecem
-		@mp.bitmap = bitmap = Cache.picture('barraenergiaCompleta')
-		@fome_bar.bitmap = bitmap = Cache.picture('barraenergiaCompleta')
-    @stamina_bar.bitmap = bitmap = Cache.picture('barraenergiaCompleta')
-    @backgroundHUD.bitmap = bitmap = Cache.picture('backgroundHUD')
-    @backgroundHUD.x = 190
-    @backgroundHUD.y = 350 
-	end
-	
-	def get_bitmap_size
-		@hpw = @hp.width
-		@hph = @hp.height
-		@mpw = @mp.width
-		@mph = @mp.height
-		@fomew = @fome_bar.width
-		@fomeh =  @fome_bar.height  
-    @staminaw = @stamina_bar.width
-		@staminah=  @stamina_bar.height  
-	end
-  
-  def draw_stamina
-    @stamina_bar.bitmap.clear
-		@stamina_bar.bitmap = Bitmap.new(@staminaw,@staminah)
-		staminaw = @staminaw *@actor.stamina/@actor.mstamina#Define the width of bar
-		staminah = @staminah
-    rect = Rect.new(0,0,staminaw,staminah)
-		@stamina_bar.bitmap.blt(0,0,Cache.picture("stamina_bar"),rect)
-		@stamina_bar.x = 206
-		@stamina_bar.y = 362
-    end
-	def draw_hp
-		@hp.bitmap.clear
-		@hp.bitmap = Bitmap.new(@hpw,@hph)
-		hpw = @hpw *@actor.hp/@actor.mhp #Define the width of bar
-		hph = @hph
-		rect = Rect.new(0,0,hpw-1,hph) #Was put "-1" to fix the value bugged
-		@hp.bitmap.blt(0,0,Cache.picture("hp_bar"),rect)
-		@hp.x = 206
-		@hp.y = 379
-    end
-	 def draw_mp
- 
-		@mp.bitmap.clear
-		@mp.bitmap = Bitmap.new(@mpw,@mph)
-		mpw = @mpw *@actor.mp / @actor.mmp
-		mph = @mph
-		rect = Rect.new(0,0,mpw,mph)
-		@mp.bitmap.blt(0,0,Cache.picture("sp_bar"),rect)
-		@mp.x = 206
-		@mp.y = 403
-     
-	end
-  
-	 def draw_fome
-		@fome_bar.bitmap.clear
-		@fome_bar.bitmap = Bitmap.new(@fomew,@fomeh)
-		fomew = @fomew *@actor.fome/@actor.mfome #Define the width of bar
-		fomeh = @fomeh
-    rect = Rect.new(0,0,fomew,fomeh)
-		@fome_bar.bitmap.blt(0,0,Cache.picture("Hunger"),rect)
-		@fome_bar.x = 206
-		@fome_bar.y = 352
-    end
-	
-	
-	 def update
-		enter_hud_update #alias
-		
-		draw_hp if hp_need_update?
-		draw_mp if mp_need_update?
-    draw_fome if fome_need_update?
-    draw_stamina if stamina_need_update?
-    @hp.visible = Input.press?(Input::A)
-    @mp.visible = Input.press?(Input::A)
-    @fome_bar.visible = Input.press?(Input::A)
-    @stamina_bar.visible = Input.press?(Input::A)
-    @backgroundHUD.visible = Input.press?(Input::A)
-	end
- 
-	
-	
-   def hp_need_update?
-		return true unless @actor_hp == @actor.hp
-		return true unless @actor_mhp == @actor.mhp
-    end
-	def mp_need_update?
-		return true unless @actor_hp == @actor.mp   
-    #Only will return "true" if the hp of hero don't be equal to itself
-    #Só vai retornar true se o hp do herói NÃO estiver igual ao do próprio herói
-		return true unless @actor_mhp == @actor.mmp 
-	end
-  
-  	def fome_need_update?
-		return true unless @actor_fome == @actor.fome  
+  def need_update?
+    hp_need_update? || mp_need_update? || stamina_need_update? || fome_need_update?
+  end
 
-		return true unless @actor_mfome == @actor.mmp 
-	end
+  def hp_need_update?
+    @actor_hp != @actor.hp || @actor_mhp != @actor.mhp
+  end
+
+  def mp_need_update?
+    @actor_mp != @actor.mp || @actor_mmp != @actor.mmp
+  end
+  
+  def fome_need_update?
+    @actor_fome != @actor.fome || @actor_mfome != @actor.mfome
+  end
+
   def stamina_need_update?
-		return true unless @actor_stamina == @actor.stamina 
+    @actor_stamina != @actor.stamina || @actor_mstamina != @actor.mstamina
+  end
 
-		return true unless @actor_mstamina == @actor.mstamina
-	end
+  def visible=(tf)
+    @bg.visible=@fg.visible=tf
+  end
+
+  def dispose
+    @bg.dispose
+    @fg.dispose
+  end
+
+end
+
+class Scene_Map < Scene_Base
+  alias enter_hud_start start
+  alias enter_hud_update update
+  alias enter_hud_terminate terminate
+
+  def start
+    enter_hud_start #alias
+    @hud = HUD.new($game_party.members[0])
+  end
 	
+  def update
+    enter_hud_update #alias
+    @hud.visible = Input.press?(:A)
+    @hud.update
+  end
 	
-	def terminate
-			enter_hud_terminate
-			dispose_hud
-	end
-	
-	def dispose_hud
-		@hp.bitmap.dispose
-		@hp.dispose
-		@mp.bitmap.dispose
-		@mp.dispose
-    @fome_bar.bitmap.dispose
-    @fome_bar.dispose
-    @stamina_bar.bitmap.dispose
-    @stamina_bar.dispose
-    @backgroundHUD.bitmap.dispose
-    @backgroundHUD.dispose
+  def terminate
+    enter_hud_terminate
+    @hud.dispose
   end
 end
