e0db728c8cc95683b324f9d9905fab60bd73c182
leolucass <leopoldolucass@gmail.com>
2015-02-08 01:06:06 -0200

    Update[System]Survivor.rb 08/02/2015

--- [System]Survivor.rb
+++ [System]Survivor.rb
@@ -1,28 +1,47 @@
-########################
-#author: leolucass(=Enter)
-########
-#module SetHUD
-#  def getMax_Stamina
- #   return @stamina_max = $game_party.members[0].mp
- # end
- # def getStamina
- #   return @stamina = 0.5*$game_party.members[0].mp
-  #  end
-  #end
+# 
+#Author: leolucas(=Enter)
+#About this script: 
+#This HUD is a script that contain HP+MP+Stamina and Hunger bar.
+#Stamina and Hunger are based in a percentage of MP/MMP.
+#RGSS3
+#Instructions: The Fome=Hunger and the Stamina are defined by percentage of MP.
+#Thus if the hero lose MP then will lose Hunger and Stamina. But if you want
+#just do decrease of Hunger or the Stamina you will need manipulate the variables
+#directly.
+# 
+#--------------- #
+#                #
+# S  E  T  U  P  #
+#                #
+#--------------- #
+module EnterConfigHUD
+
+    Fome  = 199 #, Here are defined the values to Fome/Hunger.
+
+    Mfome = 200 # Here are defined the values to Max Fome/Max Hunger.
+    
+    Stamina = 199
+    
+    Mstamina = 200
+  end
+
+#################################################
 class Game_BattlerBase
-  attr_accessor :fome, :mfome
+  attr_accessor :fome, :mfome, :stamina, :mstamina
 
   alias alsintlzgmactr initialize
 
   def initialize(*args)
     alsintlzgmactr(*args)
-    @fome = 0.5 # 5%
-    @mfome = 0.5# 5%
+    @fome = EnterConfigHUD::Fome
+    @mfome = EnterConfigHUD::Mfome
+    @stamina = EnterConfigHUD::Stamina
+    @mstamina = EnterConfigHUD::Mstamina
   end
-
+##################################################################
 end
 class Scene_Map < Scene_Base
-  #include SetHUD
+  
 	alias enter_hud_start start
 	alias enter_hud_update update
 	alias enter_hud_terminate terminate
@@ -30,8 +49,6 @@ class Scene_Map < Scene_Base
 	def start
 		enter_hud_start #alias
 		@actor = $game_party.members[0]
-    #getMax_Stamina
-    #getStamina
 		create_hud
 		update_variables
 	end
@@ -43,6 +60,7 @@ class Scene_Map < Scene_Base
 		draw_hp
 		draw_mp
 		draw_fome
+    draw_stamina
 	end
 	
 	def update_variables
@@ -52,6 +70,9 @@ class Scene_Map < Scene_Base
 		@actor_mmp = @actor.mmp
     @actor_fome = @actor.fome
     @actor_mfome = @actor.mfome
+    @actor_stamina = @actor.stamina
+    @actor_mstamina = @actor.mstamina
+
 		
 	end
 	
@@ -59,11 +80,17 @@ class Scene_Map < Scene_Base
 		@hp = Sprite.new
 		@mp = Sprite.new
 		@fome_bar = Sprite.new
+    @stamina_bar = Sprite.new
+    @backgroundHUD = Sprite.new
 	end
 	def create_bitmaps
-		@hp.bitmap = bitmap = Cache.picture('barraenergia')
-		@mp.bitmap = bitmap = Cache.picture('barraenergia')
-		@fome_bar.bitmap = bitmap = Cache.picture('barraenergia')
+		@hp.bitmap = bitmap = Cache.picture('barraenergiaCompleta')#Verificar isso, quando apaga ela e coloca outra, as barras não aparecem
+		@mp.bitmap = bitmap = Cache.picture('barraenergiaCompleta')
+		@fome_bar.bitmap = bitmap = Cache.picture('barraenergiaCompleta')
+    @stamina_bar.bitmap = bitmap = Cache.picture('barraenergiaCompleta')
+    @backgroundHUD.bitmap = bitmap = Cache.picture('backgroundHUD')
+    @backgroundHUD.x = 190
+    @backgroundHUD.y = 350 
 	end
 	
 	def get_bitmap_size
@@ -72,41 +99,53 @@ class Scene_Map < Scene_Base
 		@mpw = @mp.width
 		@mph = @mp.height
 		@fomew = @fome_bar.width
-		@fomeh = @mp.height
+		@fomeh =  @fome_bar.height  
+    @staminaw = @stamina_bar.width
+		@staminah=  @stamina_bar.height  
 	end
-	
+  
+  def draw_stamina
+    @stamina_bar.bitmap.clear
+		@stamina_bar.bitmap = Bitmap.new(@staminaw,@staminah)
+		staminaw = @staminaw *@actor.stamina/@actor.mstamina#Define the width of bar
+		staminah = @staminah
+    rect = Rect.new(0,0,staminaw,staminah)
+		@stamina_bar.bitmap.blt(0,0,Cache.picture("stamina_bar"),rect)
+		@stamina_bar.x = 206
+		@stamina_bar.y = 362
+    end
 	def draw_hp
 		@hp.bitmap.clear
 		@hp.bitmap = Bitmap.new(@hpw,@hph)
-		hpw = @hpw * @actor.hp/@actor.mhp #Define a largura da barra
+		hpw = @hpw *@actor.hp/@actor.mhp #Define the width of bar
 		hph = @hph
-		rect = Rect.new(0,0,hpw,hph)
-		@hp.bitmap.blt(0,0,Cache.picture("barraenergiaCompleta"),rect)
-		@hp.x = 205
-		@hp.y = 380
+		rect = Rect.new(0,0,hpw-1,hph) #Was put "-1" to fix the value bugged
+		@hp.bitmap.blt(0,0,Cache.picture("hp_bar"),rect)
+		@hp.x = 206
+		@hp.y = 379
     end
 	 def draw_mp
  
 		@mp.bitmap.clear
 		@mp.bitmap = Bitmap.new(@mpw,@mph)
-		mpw = @mpw * @actor.mp / @actor.mmp
+		mpw = @mpw *@actor.mp / @actor.mmp
 		mph = @mph
 		rect = Rect.new(0,0,mpw,mph)
-		@mp.bitmap.blt(0,0,Cache.picture("barraenergiaCompleta"),rect)
-		@mp.x = 205
-		@mp.y = 400
+		@mp.bitmap.blt(0,0,Cache.picture("sp_bar"),rect)
+		@mp.x = 206
+		@mp.y = 403
      
 	end
   
 	 def draw_fome
 		@fome_bar.bitmap.clear
-		@fome_bar.bitmap = Bitmap.new(200,400)
-		fomew = @fomew *((@actor.fome*@actor.mp)/(@actor.mfome*@actor.mmp)) #Define a largura da barra
+		@fome_bar.bitmap = Bitmap.new(@fomew,@fomeh)
+		fomew = @fomew *@actor.fome/@actor.mfome #Define the width of bar
 		fomeh = @fomeh
     rect = Rect.new(0,0,fomew,fomeh)
-		@fome_bar.bitmap.blt(0,0,Cache.picture("barraenergiaCompleta"),rect)
-		@fome_bar.x = 205
-		@fome_bar.y = 320
+		@fome_bar.bitmap.blt(0,0,Cache.picture("Hunger"),rect)
+		@fome_bar.x = 206
+		@fome_bar.y = 352
     end
 	
 	
@@ -116,11 +155,14 @@ class Scene_Map < Scene_Base
 		draw_hp if hp_need_update?
 		draw_mp if mp_need_update?
     draw_fome if fome_need_update?
-
-		 
+    draw_stamina if stamina_need_update?
+    @hp.visible = Input.press?(Input::A)
+    @mp.visible = Input.press?(Input::A)
+    @fome_bar.visible = Input.press?(Input::A)
+    @stamina_bar.visible = Input.press?(Input::A)
+    @backgroundHUD.visible = Input.press?(Input::A)
 	end
-	
-	
+ 
 	
 	
    def hp_need_update?
@@ -128,15 +170,21 @@ class Scene_Map < Scene_Base
 		return true unless @actor_mhp == @actor.mhp
     end
 	def mp_need_update?
-		return true unless @actor_hp == @actor.mp  #Só vai retornar true se o hp do herói NÃO estiver igual ao do próprio herói
-
-		return true unless @actor_mhp == @actor.mmp #do máximo 
+		return true unless @actor_hp == @actor.mp   
+    #Only will return "true" if the hp of hero don't be equal to itself
+    #Só vai retornar true se o hp do herói NÃO estiver igual ao do próprio herói
+		return true unless @actor_mhp == @actor.mmp 
 	end
   
-  	def fome_need_update? #*está dando erro, pois vc precisa associar ao heroi, para que o evento diminua o valor dessa variavel
-		return true unless @actor_fome == @actor.fome  #Só vai retornar true se o hp do herói NÃO estiver igual ao do próprio herói
+  	def fome_need_update?
+		return true unless @actor_fome == @actor.fome  
 
-		return true unless @actor_mfome == @actor.mmp #do máximo 
+		return true unless @actor_mfome == @actor.mmp 
+	end
+  def stamina_need_update?
+		return true unless @actor_stamina == @actor.stamina 
+
+		return true unless @actor_mstamina == @actor.mstamina
 	end
 	
 	
@@ -147,10 +195,14 @@ class Scene_Map < Scene_Base
 	
 	def dispose_hud
 		@hp.bitmap.dispose
-		@hp.dispose##
+		@hp.dispose
 		@mp.bitmap.dispose
 		@mp.dispose
     @fome_bar.bitmap.dispose
     @fome_bar.dispose
+    @stamina_bar.bitmap.dispose
+    @stamina_bar.dispose
+    @backgroundHUD.bitmap.dispose
+    @backgroundHUD.dispose
   end
 end
